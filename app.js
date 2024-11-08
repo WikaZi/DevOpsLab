8d63cb0ca9d488984cdb8978158739f9293ffd8f
WikaZi <142423027+WikaZi@users.noreply.github.com>
2024-11-08 14:44:16 +0100

    docker added

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
-
 const port = 3000;
+
+
+const db = mysql.createConnection({
+  host: 'mysql',       
+  user: 'user',       
+  password: 'password', 
+  database: 'mydatabase' 
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych:', err);
+    return;
+  }
+  console.log('Połączono z bazą danych MySQL');
+});
 
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+
+app.get('/db', (req, res) => {
+  db.query('SELECT NOW()', (err, results) => {
+    if (err) {
+      res.status(500).send('Błąd połączenia z bazą danych');
+      return;
+    }
+    res.send(`Data z bazy danych: ${results[0]['NOW()']}`);
+  });
+});
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
